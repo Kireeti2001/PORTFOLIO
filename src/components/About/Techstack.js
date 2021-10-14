b691d7f152d8a782f43a3eb0a63159f073817e97
Kireeti_Nittala <kireeti.nittala@gmail.com>
2021-10-14 14:50:01 +0530

    .

--- src/components/About/Techstack.js
+++ src/components/About/Techstack.js
@@ -22,6 +22,9 @@ function Techstack() {
       <Col xs={4} md={2} className="tech-icons">
         <DiGit />
       </Col>
+      <Col xs={4} md={2} className="tech-icons">
+        <DiGit />
+      </Col>
     </Row>
   );
 }
